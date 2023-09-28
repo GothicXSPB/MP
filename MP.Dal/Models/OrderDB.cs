1f8b60073fe581ba45373ab6605633348db8afd0
GothicXSPB <ZZZerggg@mail.ru>
2023-09-28 21:54:12 +0300

    add order model and PVZ

--- MP.Dal/Models/OrderDB.cs
+++ MP.Dal/Models/OrderDB.cs
@@ -1,6 +1,17 @@
-﻿namespace Marketplace.Dal.Models
+﻿namespace MP.Dal.Models
 {
     public class OrderDB
-    {
+    { 
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public decimal Price { get; set; }
+
+        public bool IsDeleted { get; set; }
+        public bool IsPVZ { get; set; }
+        public bool IsCourier { get; set; } 
+        public DateTime GetOnPVZ {  get; set; }
+        public DateTime GetOnCourier { get; set; }
+        public DateTime GetOnStorage { get; set; }
+
     }
 }
